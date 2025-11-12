176e9ab2144338b3761c44abaec557a6a4635228
Enoch <enochekele5@gmail.com>
2025-11-12 11:28:29 +0100

    mobile home done

--- src/home/Body.jsx
+++ src/home/Body.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import homeHero from '../assets/homeHero.png'
-import { Play, Star, StarHalf, Users, DollarSign, Stars, CircleArrowRight, MoveRight, ChevronRight } from 'lucide-react';
+import { Play, Star, StarHalf, Users, DollarSign, Stars, CircleArrowRight, MoveRight, ChevronRight, Linkedin, Instagram, Facebook } from 'lucide-react';
 import femaleModel from '@/assets/femaleModel.jpg'
 import maleModel from '@/assets/maleModel.jpg'
 import border1 from '@/assets/border1.png'
@@ -9,6 +9,7 @@ import course1 from '@/assets/course1.jpg'
 import course2 from '@/assets/course2.jpg'
 import course3 from '@/assets/course3.jpg'
 import course4 from '@/assets/course4.png'
+import edgex from '@/assets/edgex.png'
 
 
 function Body() {
@@ -260,7 +261,7 @@ function Body() {
                         <p className='text-[#EB7F3A] text-md font-[NeoGram Trial]'>TESTIMONIAL</p>
                     </div>
                     <div className='text-justify space-y-4 text-[.5rem]  '>
-                        <p className='text-[#34ACB4] text-md font-bold'>What They Say?</p>
+                        <p className='text-[#34ACB4] text-xl '>What They Say?</p>
 
                         <p>edgeX has earned the trust of users across the globe, with more than 100,000 positive ratings.</p>
                         <p>Some students and teachers have benefited greatly from using edgeX, finding it an invaluable support in their learning and teaching journey.</p>
@@ -284,11 +285,41 @@ function Body() {
                                 <Star className='fill-[#EB7F3A] text-[#EB7F3A] w-2 h-2' />
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
 
             </div>
+            <div className='bg-[#E0ECFF] w-[calc(100%+2rem)] h-25 -ml-4 flex flex-col items-center justify-center gap-1'>
+
+                <div className='w-fit'>
+                    <img src={edgex} alt="logo" className='w-20 h-15' />
+                </div>
+
+                <div className='text-center text-[.5rem] font-bold flex gap-1.5'>
+                    <span>Home</span>|<span>Courses</span>|<span>About Us</span>|<span>Subscription</span>|<span>log in</span>|<span>Sign up</span>
+                </div>
+                <div className='flex items-center justify-center gap-2'>
+                    {/* Google icon - lucide-react doesn't have a Google icon, using a generic representation */}
+                    <svg className='w-3 h-3 text-black fill-black' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="currentColor" />
+                        <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="currentColor" />
+                        <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="currentColor" />
+                        <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="currentColor" />
+                    </svg>
+                    <Linkedin className='w-3 h-3 text-black fill-black' />
+                    <Instagram className='w-3 h-3 text-black' />
+                    <Facebook className='w-3 h-3 text-black fill-white bg-black' />
+                </div>
+
+
+            </div>
+            <div className='flex justify-between text-[.5rem] mt-4'>
+                <p>(c) 2025 copyright | edgeX, All rights reserved</p>
+                <p>Privacy & Policy    |    Terms & Condition</p>
+            </div>
+            
+
 
 
 
